f446d743fa8fba90229b5bb39981c6741858b741
“githubvishal1234” <kondaparthyvishal6@gmail.com>
2025-01-15 19:30:40 +0530

    3rd commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+name="src",
+version="0.0.1",
+author="vishal",
+author_email="kondaparthyvishal6@gmail.com",
+packages=find_packages(),
+install_requires=[],
+)
